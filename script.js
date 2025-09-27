f9b81a8bb1941d1b843b94a4c9a0c3edc5b024bc
tuongnq2793 <tuong.nq.2793@aptechlearning.edu.vn>
2025-09-27 17:45:32 +0700

    feat: initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function sayHello() {
+  let name = document.getElementById("name").value;
+  alert("Xin chao, " + name);
+}
+function sayHello() {
+  let name = document.getElementById("name").value;
+  alert("Xin chao, " + name);
+}
